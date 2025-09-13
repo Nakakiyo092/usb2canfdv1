1331cf583b16013c17cfa2e8a616ae5db70519bf
NAKANISHI Kiyomaro <kiyomaro092@gmail.com>
2025-09-13 14:59:48 +0900

    Organize source code

--- usb2canfdv1-fw/Slcan/Src/buffer.c
+++ usb2canfdv1-fw/Slcan/Src/buffer.c
@@ -73,9 +73,9 @@ void buf_process(void)
 {
     // Process cdc receive buffer
     __disable_irq();
-    uint32_t tmp_head = buf_cdc_rx.head;
+    uint8_t data_ready = (buf_cdc_rx.tail != buf_cdc_rx.head);
     __enable_irq();
-    if (buf_cdc_rx.tail != tmp_head)
+    if (data_ready)
     {
         //  Process one whole buffer
         for (uint32_t i = 0; i < buf_cdc_rx.msglen[buf_cdc_rx.tail]; i++)
