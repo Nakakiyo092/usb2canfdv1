54feb2e3936ea4a3748b4550f41226a5c5ed9aee
Nakakiyo092 <kiyomaro092@gmail.com>
2026-02-05 00:19:24 +0900

    Add comment about timestamp

--- usb2canfdv1-fw/Slcan/Src/generator.c
+++ usb2canfdv1-fw/Slcan/Src/generator.c
@@ -213,6 +213,7 @@ uint16_t slcan_generate_tx_event(uint8_t *buf, FDCAN_TxEventFifoTypeDef *tx_even
 
 
 // Gets milli second timestamp for the current time (2bytes, Resets at 60,000ms)
+// This implementation will breake if the timesatamp is not calculated for more than HAL_GetTick overflow (~49 days, or twice?).
 uint16_t slcan_get_timestamp_ms(void)
 {
     static uint16_t slcan_last_timestamp_ms = 0;
@@ -230,8 +231,11 @@ uint16_t slcan_get_timestamp_ms(void)
 }
 
 // Gets micro second timestamp for the tim3 clock (4bytes, Resets at 3600,000,000us)
+// This implementation will breake if the timesatamp is not calculated for more than HAL_GetTick overflow (~49 days, or twice?).
 // The tim3_us does not have to be the current value but supposed to be close to it (like ~1ms).
 // The difference between the current tim3 value and tim3_us should never be more than UINT16_MAX / 2 ~ 30ms.
+// This is supported by the fact the observed maximum loop cycle time is about 300us.
+// TODO: Implement check for the main loop and raise error if it is too large?
 uint32_t slcan_get_timestamp_us_from_tim3(uint16_t tim3_us)
 {
     static uint32_t slcan_last_timestamp_us = 0;
