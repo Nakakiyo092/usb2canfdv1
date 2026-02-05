18f19e536030a3f1f9dae6ed196a615347a3cf78
Nakakiyo092 <kiyomaro092@gmail.com>
2026-02-05 23:41:50 +0900

    Improve CDC Rx buffer overflow handling

--- usb2canfdv1-fw/USB_Device/App/usbd_cdc_if.c
+++ usb2canfdv1-fw/USB_Device/App/usbd_cdc_if.c
@@ -268,6 +268,7 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
+  static uint8_t overflow_flag = 0;
   uint32_t new_head = (buf_cdc_rx.head + 1) % BUF_CDC_RX_NUM_BUFS;
   if (new_head == buf_cdc_rx.tail)
   {
@@ -275,13 +276,20 @@ static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
     slcan_raise_error(SLCAN_STS_CAN_TX_FIFO_FULL);
 
     // Listen again on the same buffer. Old data will be overwritten.
-    // TODO: Delete corrupted data i.e. all data from last [CR] till next [CR]
     USBD_CDC_SetRxBuffer(&hUsbDeviceFS, (uint8_t *)buf_cdc_rx.data[buf_cdc_rx.head]);
     USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+    overflow_flag = 1;
     return (USBD_FAIL);
   }
   else
   {
+    // Mark corrupted data i.e. all data around overflow from last [CR] till next [CR]
+    if (overflow_flag)
+    {
+      buf_cdc_rx.data[buf_cdc_rx.head][0] = '\a';    // Mark as invalid (\a = [BELL])
+      overflow_flag = 0;
+    }
+
     // Save length and move to next buffer
     buf_cdc_rx.msglen[buf_cdc_rx.head] = *Len;
     buf_cdc_rx.head = new_head;
