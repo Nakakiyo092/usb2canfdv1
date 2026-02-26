850eaab1bc542dc5e33e23d43159c530e285594c
Nakakiyo092 <kiyomaro092@gmail.com>
2026-02-27 01:50:19 +0900

    Refactor speed test script

--- test/cdc_speed_test.py
+++ test/cdc_speed_test.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
-import argparse
-import serial
+"""
+Test USB CDC throughput.
+
+License:
+    MIT License.
+    See the accompanying LICENSE file for full terms.
+"""
+
 import time
 import threading
 
+import argparse
+import serial
+
 def get_argparser():
+    """Get argument parser for this script."""
     parser = argparse.ArgumentParser(
         description="Test USB CDC throughput. Press 'q' + [ENTER] to quit."
     )
@@ -41,13 +51,35 @@ def get_argparser():
 
 
 def check_key():
+    """Check if user input 'q' to quit the test."""
     while True:
         if input() == 'q':
             print("quit now")
             break
 
 
+def print_test_environment(dev: serial):
+    """Print test environment information."""
+    print("usb port name:", dev.port)
+    print("")
+
+    dev.write(b"N\r")
+    time.sleep(0.1)
+    print("serial number:", dev.read_all().decode())
+    dev.write(b"V\r")
+    time.sleep(0.1)
+    print("slcan version:", dev.read_all().decode())
+    dev.write(b"v\r")
+    time.sleep(0.1)
+    print("detail:")
+    print("   ", dev.read_all().decode())
+    print("")
+
+    print("can port status: closed")
+
+
 def make_data_to_write(mode: str, chunk_size: int) -> bytes:
+    """Make data to write to device."""
     if mode == "tx":
         single_msg = b"00112233445566778899AABBCCDDEEFF"
         single_msg = b"B00000000F" + single_msg + single_msg + single_msg + single_msg + b"\r"
@@ -61,32 +93,77 @@ def make_data_to_write(mode: str, chunk_size: int) -> bytes:
     return data_write
 
 
+def count_message(data: bytes) -> int:
+    """Count the number of messages in the data."""
+    targets = {ord("\r"), ord("\a")}
+    return sum(byte in targets for byte in data)
+
+
+def print_speed_and_loss(stats: dict, duration: int):
+    """Print the speed and message loss."""
+    if duration == 0:
+        print("tx speed: ", "N/A")
+        print("rx speed: ", "N/A")
+    else:
+        tx_speed = stats["tx_len"] / 1000 / duration
+        rx_speed = stats["rx_len"] / 1000 / duration
+        print("tx speed: ", f"{tx_speed:8.2f}", "kB/s\t", f"{tx_speed * 8:8.2f}", "kbits/s")
+        print("rx speed: ", f"{rx_speed:8.2f}", "kB/s\t", f"{rx_speed * 8:8.2f}", "kbits/s")
+
+    print("message loss: ", stats["tx_msg"] - stats["rx_msg"], " / ", stats["tx_msg"])
+
+
+def print_device_status(dev: serial):
+    """Print device status information."""
+    dev.write(b"F\r")
+    time.sleep(0.1)
+    print("device status:", dev.read_all().decode())
+    print("detail:")
+
+    dev.write(b"f\r")
+    time.sleep(0.1)
+    resp = dev.read_all().decode()
+    if resp != "\a":
+        print("   ", resp)
+
+    dev.write(b"z\r")
+    time.sleep(0.1)
+    resp = dev.read_all().decode()
+    if resp != "\a":
+        print("   ", resp)
+
+
 def main():
+    """Main function."""
     argparser = get_argparser()
     args = argparser.parse_args()
 
     try:
         device = serial.Serial(args.devicename, timeout=1, write_timeout=1)
-    except Exception as e:
+    except Exception as err:
         print("ERROR: Could not open device ", args.devicename)
-        print(e)
+        print(err)
         return
 
     check_key_thread = threading.Thread(target=check_key)
     check_key_thread.start()
 
+    device.write(b"\a\r\r")
     device.write(b"C\r")
     time.sleep(0.1)
     device.read_all()
-    print("port_closed ", device.port)
+
+    print_test_environment(device)
     print("")
 
     data_write = make_data_to_write("tx" if args.tx else "rx", args.chunk_size)
 
-    tx_len = 0
-    rx_len = 0
-    tx_cnt = 0
-    rx_cnt = 0
+    stats = {
+        "tx_len": 0,
+        "rx_len": 0,
+        "tx_msg": 0,
+        "rx_msg": 0,
+    }
 
     flag_tx = True
 
@@ -95,49 +172,36 @@ def main():
     while True:
         if flag_tx:
             device.write(data_write)
-            tx_len += len(data_write)
-            tx_cnt += args.chunk_size
+            stats["tx_len"] += len(data_write)
+            stats["tx_msg"] += args.chunk_size
 
         data_read = device.read_all()
-        rx_len += len(data_read)
-        for byte in data_read:
-            if bytes([byte]) == b"\r" or bytes([byte]) == b"\a":
-                rx_cnt += 1
+        stats["rx_len"] += len(data_read)
+        stats["rx_msg"] += count_message(data_read)
 
         ms = int(round(time.time() * 1000))
         if ms > tick_next and not flag_tx:
-            tx_speed = tx_len / 1000 / args.duration
-            rx_speed = rx_len / 1000 / args.duration
-            print("tx speed: ", f"{tx_speed:8.2f}", "kB/s\t", f"{tx_speed * 8:8.2f}", "kbits/s")
-            print("rx speed: ", f"{rx_speed:8.2f}", "kB/s\t", f"{rx_speed * 8:8.2f}", "kbits/s")
-            print("message loss: ", tx_cnt - rx_cnt, " / ", tx_cnt)
+            print_speed_and_loss(stats, args.duration)
             print("")
-            #device.write(b"F\r")
-            #time.sleep(0.1)
-            print("device status: ", device.read_all().decode())
-            #device.write(b"f\r")
-            #time.sleep(0.1)
-            #print(device.read_all().decode())
-            device.write(b"z\r")
-            time.sleep(0.1)
-            print(device.read_all().decode())
+            print_device_status(device)
             print("")
 
-            tx_len = 0
-            rx_len = 0
-            tx_cnt = 0
-            rx_cnt = 0
-            
+            for key in stats:
+                stats[key] = 0
+
             ms = int(round(time.time() * 1000))
             tick_next = ms + 1000 * args.duration
             flag_tx = True
-        
-            if check_key_thread.is_alive() == False:
+
+            if check_key_thread.is_alive() is False:
                 break
+
         elif ms > tick_next and flag_tx:
-            tick_next = ms + 1000 
+            tick_next = ms + 1000
             flag_tx = False
 
+    device.write(b"C\r")
+    time.sleep(0.1)
     device.read_all()
     device.close()
 
