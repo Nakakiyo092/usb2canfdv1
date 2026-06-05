1d4d14c9913908f8dfaea67241b463288ffb7c98
NAKANISHI Kiyomaro <kiyomaro092@gmail.com>
2026-06-05 20:52:39 +0900

    Regenerate code

--- usb2canfdv1-fw/Core/Src/main.c
+++ usb2canfdv1-fw/Core/Src/main.c
@@ -97,6 +97,7 @@ int main(void)
   MX_FDCAN1_Init();
   MX_USB_Device_Init();
   MX_TIM3_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   led_init();
   buf_init();
@@ -182,8 +183,7 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
