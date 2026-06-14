0cdc69dd02c26f8dec800bb532f1d706beff7636
NAKANISHI Kiyomaro <kiyomaro092@gmail.com>
2026-06-14 18:27:51 +0900

    Increment fw version to 2.2

--- usb2canfdv1-fw/App/Src/parser.c
+++ usb2canfdv1-fw/App/Src/parser.c
@@ -29,8 +29,8 @@
 #include "bootloader.h"
 #endif
 
-#define SLCAN_VERSION       "VW1K4"
-#define SLCAN_SW_VERSION    "2.1.0"
+#define SLCAN_VERSION       "VW1K5"
+#define SLCAN_SW_VERSION    "2.2.0"
 #define SLCAN_RET_OK    ((uint8_t*)"\r")
 #define SLCAN_RET_ERR   ((uint8_t*)"\a")
 #define SLCAN_RET_LEN   1U
